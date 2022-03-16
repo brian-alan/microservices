a7cc9b374a95b281c2bc79ff3bd4504007f86c2f
brian-alan <brian-alan.castro@atos.net>
2022-03-16 12:02:31 -0600

    Added Multiple Instances

--- client-service/client-service/src/main/java/com/microservices/clientservice/feignclients/CardFeignClient.java
+++ client-service/client-service/src/main/java/com/microservices/clientservice/feignclients/CardFeignClient.java
@@ -4,14 +4,11 @@ import com.microservices.clientservice.dto.CardDTO;
 import com.microservices.clientservice.model.Card;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name = "payment-service", url = "http://localhost:9081/cards")
+@FeignClient(name = "payment-service", path = "/cards")
 public interface CardFeignClient {
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
