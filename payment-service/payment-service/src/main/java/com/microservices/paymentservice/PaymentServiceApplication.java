70e25c49784faf5d5497466e96b50d0187abd177
brian-alan <brian-alan.castro@atos.net>
2022-03-16 10:41:21 -0600

    Added Eureka

--- payment-service/payment-service/src/main/java/com/microservices/paymentservice/PaymentServiceApplication.java
+++ payment-service/payment-service/src/main/java/com/microservices/paymentservice/PaymentServiceApplication.java
@@ -3,8 +3,10 @@ package com.microservices.paymentservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@EnableEurekaClient
 public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
