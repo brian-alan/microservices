ab2e0b6c839bf1bd80f59bfbd0a1383e1cc5046c
brian-alan <brian-alan.castro@atos.net>
2022-03-16 09:54:47 -0600

    Added config-service

--- client-service/client-service/src/main/java/com/microservices/clientservice/feignclients/CardFeignClient.java
+++ client-service/client-service/src/main/java/com/microservices/clientservice/feignclients/CardFeignClient.java
@@ -4,7 +4,6 @@ import com.microservices.clientservice.dto.CardDTO;
 import com.microservices.clientservice.model.Card;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
