20ced76c7b0a9ddc8d8a95b741a88a5105319d99
brian-alan <brian-alan.castro@atos.net>
2022-03-17 15:54:44 -0600

    Changes in Videogame microservice functionality

--- videogame-service/videogame-service/src/main/java/com/microservices/videogameservice/repository/VideogameRepository.java
+++ videogame-service/videogame-service/src/main/java/com/microservices/videogameservice/repository/VideogameRepository.java
@@ -2,6 +2,8 @@ package com.microservices.videogameservice.repository;
 
 import com.microservices.videogameservice.entity.Videogame;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VideogameRepository extends JpaRepository<Videogame, Integer> {
 }
